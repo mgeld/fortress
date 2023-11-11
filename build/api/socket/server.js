2a001602038423a84d9f2830e94656b1abe99ae0
mgeld <mgeld98@gmail.com>
2023-11-11 12:21:42 +0300

    111

--- build/api/socket/server.js
+++ build/api/socket/server.js
@@ -66,7 +66,8 @@ let Server = class Server {
             let router = serverContext._handlers.handle(ws);
             ws.on('message', router);
         });
-        const hostname = '192.168.43.90';
+        const hostname = '89.108.71.67';
+        console.log('hostname', hostname);
         server.listen(8080, hostname, () => console.log('Htpsssss'));
     }
 };
