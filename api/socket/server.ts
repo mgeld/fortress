a3522a1f54df3dfa6ff5cc19508f55656764a303
mgeld <mgeld98@gmail.com>
2023-11-11 12:10:54 +0300

    new

--- api/socket/server.ts
+++ api/socket/server.ts
@@ -106,7 +106,7 @@ export class Server {
         // connection.start()
         // const interval = setInterval(() => connection.pingPong(), 5000);
 
-        const hostname = '192.168.43.90'
+        const hostname = '89.108.71.67'
 
         server.listen(8080, hostname, () => console.log('Htpsssss'));
 
